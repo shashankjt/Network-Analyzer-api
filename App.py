b396babb19739cd86f9ca95d07a69ac821a2bc53
shashankjt <shashankjt11@gmail.com>
2025-07-19 20:49:08 +0530

    Initial commit

---
+++ App.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, jsonify
+import pickle
+import numpy as np
+
+app = Flask(__name__)
+model = pickle.load(open("model.pkl", "rb"))
+
+@app.route("/")
+def home():
+    return " Network Analyzer model is running!"
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    data = request.get_json(force=True)
+
+    # Example input: {"features": [12, 34, 1, 400, 1234.5, 2]}
+    features = np.array(data["features"]).reshape(1, -1)
+    prediction = model.predict(features)[0]
+
+    return jsonify({"prediction": int(prediction)})
+
+if __name__ == "__main__":
+    app.run(debug=True)
